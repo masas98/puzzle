0a99f13632e7dde9f6bfafdaceb82e61eb4ffabf
masas98 <92392734+masas98@users.noreply.github.com>
2024-09-04 21:49:54 +0300

    Add files via upload

---
+++ gateway-service/src/main/java/com/example/gatewayservice/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.example.gatewayservice;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.netty.http.client.HttpClient;
+
+public class CorsConfig extends CorsConfiguration {
+
+  @Bean
+  public CorsWebFilter corsFilter() {
+    org.springframework.web.cors.CorsConfiguration corsConfiguration =
+        new org.springframework.web.cors.CorsConfiguration();
+    corsConfiguration.addAllowedOrigin("*");
+    corsConfiguration.setAllowedMethods(
+        Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+    corsConfiguration.addAllowedHeader("origin");
+    corsConfiguration.addAllowedHeader("content-type");
+    corsConfiguration.addAllowedHeader("accept");
+    corsConfiguration.addAllowedHeader("authorization");
+    corsConfiguration.addAllowedHeader("cookie");
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return new CorsWebFilter(source);
+  }
+
+  @Bean
+  public HttpClient httpClient() {
+    return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+  }
+
+}
